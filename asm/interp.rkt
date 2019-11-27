d7edb0ca57046089c1690a41ba53667db32c346a
David Van Horn <dvanhorn@cs.umd.edu>
2019-11-27 10:14:47 -0500

    Close ports.

--- asm/interp.rkt
+++ asm/interp.rkt
@@ -3,16 +3,16 @@
 (require "printer.rkt" racket/runtime-path)
 (define-runtime-path dir "..")
 
-;; Asm -> Integer
+;; Asm -> String
 ;; Interpret (by assemblying, linking, and exec'ing) x86-64 code
 ;; Assume: starts with entry point run-time expects
 (define (asm-interp a)
-  (asm-interp/input a ""))
+  (with-input-from-string (asm-interp/io a "") read))
 
-;; Asm String -> Integer
+;; Asm String -> String
 ;; Interpret (by assemblying, linking, and exec'ing) x86-64 code
 ;; Assume: starts with entry point run-time expects
-(define (asm-interp/input a str)
+(define (asm-interp/io a str)
   (let* ((t.s (make-temporary-file "nasm~a.s"))
          (t.run (path-replace-extension t.s #".run")))
     (with-output-to-file t.s
@@ -30,4 +30,4 @@
        (begin
          (status 'wait)
          (delete-file t.run)
-         (read in))])))
+         (port->string in #:close? #t))])))
